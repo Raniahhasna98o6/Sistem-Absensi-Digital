1152060b9332ca762198b63753a858a8efa78364
Raniahhasna98o6 <raniahhf@gmail.com>
2026-04-16 00:41:17 +0700

    update

--- backend-absensi/models/laporan.go
+++ backend-absensi/models/laporan.go
@@ -1,18 +1,24 @@
 package models
+import "backend-absensi/config"
 
 type Laporan struct {
-	id_laporan int
-	periode    string
+	IDLaporan int    `json:"id_laporan"`
+	IDUser    string `json:"id_user"`
+	Periode   string `json:"periode"`
 }
+func GetLaporan() ([]Laporan, error) {
+	rows, err := config.DB.Query("SELECT id_laporan, id_user, periode FROM Laporan")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
 
-func (l *Laporan) GenerateLaporan() Laporan {
-	return *l
-}
+	var list []Laporan
+	for rows.Next() {
+		var l Laporan
+		rows.Scan(&l.IDLaporan, &l.IDUser, &l.Periode)
+		list = append(list, l)
+	}
 
-func (l *Laporan) TampilkanLaporan() []Absensi {
-	return []Absensi{}
-}
-
-func (l *Laporan) ExportPDF() string {
-	return "file.pdf"
+	return list, nil
 }
