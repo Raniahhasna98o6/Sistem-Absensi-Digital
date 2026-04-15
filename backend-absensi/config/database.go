725a050f17512e8aee9388330e024e2e59969d13
Raniahhasna98o6 <raniahhf@gmail.com>
2026-04-16 00:38:38 +0700

    update

--- backend-absensi/config/database.go
+++ backend-absensi/config/database.go
@@ -1,26 +1,25 @@
 package config
-
 import (
 	"database/sql"
 	"log"
-
+	"time"
 	_ "github.com/go-sql-driver/mysql"
 )
-
 var DB *sql.DB
 
 func ConnectDB() {
-	dsn := "root:password@tcp(127.0.0.1:3306)/absensi_db"
+	dsn := "root:nA^94eV1sha@tcp(127.0.0.1:3307)/IMPAL?parseTime=true"
 	db, err := sql.Open("mysql", dsn)
 	if err != nil {
 		log.Fatal("Gagal connect database:", err)
 	}
-
 	err = db.Ping()
 	if err != nil {
 		log.Fatal("Database tidak merespon:", err)
 	}
-
+	db.SetMaxOpenConns(10)
+	db.SetMaxIdleConns(5)
+	db.SetConnMaxLifetime(time.Hour)
 	DB = db
 	log.Println("Database connected")
 }
