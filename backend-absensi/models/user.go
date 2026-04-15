525b44de1659660bcbd9ebddd7b34c784b709ada
Raniahhasna98o6 <raniahhf@gmail.com>
2026-04-16 00:39:11 +0700

    update

--- backend-absensi/models/user.go
+++ backend-absensi/models/user.go
@@ -1,56 +1,72 @@
 package models
-
+import (
+	"backend-absensi/config"
+)
 type User struct {
-	id_user  string
-	nama     string
-	email    string
-	password string
+	ID       string `json:"id_user"`
+	Nama     string `json:"nama"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
 }
-
 // constructor
 func NewUser(id, nama, email, password string) *User {
 	return &User{
-		id_user:  id,
-		nama:     nama,
-		email:    email,
-		password: password,
+		ID:       id,
+		Nama:     nama,
+		Email:    email,
+		Password: password,
 	}
 }
-
+// cek password
+func (u *User) CheckPassword(password string) bool {
+	return u.Password == password
+}
 // getter
 func (u *User) GetIDUser() string {
-	return u.id_user
+	return u.ID
 }
 func (u *User) GetNama() string {
-	return u.nama
+	return u.Nama
 }
 func (u *User) GetEmail() string {
-	return u.email
+	return u.Email
 }
 
 // setter
-func (u *User) setNama(nama string) {
-	u.nama = nama
+func (u *User) SetNama(nama string) {
+	u.Nama = nama
 }
-func (u *User) setEmail(email string) {
-	u.email = email
+func (u *User) SetEmail(email string) {
+	u.Email = email
 }
-func (u *User) setPassword(password string) {
-	u.password = password
+func (u *User) SetPassword(password string) {
+	u.Password = password
 }
 
 // method
 func (u *User) Login(email string, password string) bool {
-	return u.email == email && u.password == password
+	return u.Email == email && u.Password == password
 }
-
 func (u *User) Logout() {}
-
 func (u *User) UpdateProfil(nama string, email string) {
-	u.nama = nama
-	u.email = email
+	u.Nama = nama
+	u.Email = email
+}
+func (u *User) GantiPassword(passwordBaru string) {
+	u.Password = passwordBaru
 }
 
-func (u *User) GantiPassword(passwordBaru string) {
-	u.password = passwordBaru
+// ambil user dari database
+func GetUserByEmail(email string) (*User, error) {
+	var user User
+
+	query := "SELECT id_user, nama, email, password FROM User WHERE email = ?"
+	row := config.DB.QueryRow(query, email)
+
+	err := row.Scan(&user.ID, &user.Nama, &user.Email, &user.Password)
+	if err != nil {
+		return nil, err
+	}
+
+	return &user, nil
 }
