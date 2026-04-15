5fa560bca21fe5d1562fc818c3d62c9db21f4bcb
Raniahhasna98o6 <raniahhf@gmail.com>
2026-04-16 00:39:51 +0700

    update

--- backend-absensi/models/dosen.go
+++ backend-absensi/models/dosen.go
@@ -1,20 +1,18 @@
 package models
+import "backend-absensi/config"
 
 type Dosen struct {
-	User
-	nidn string
+	IDUser string `json:"id_user"`
+	NIDN   string `json:"nidn"`
 }
 
-func (d *Dosen) TambahMataKuliah(matkul MataKuliah) {}
+func GetDosenByID(id string) (*Dosen, error) {
+	var d Dosen
+	query := "SELECT id_user, nidn FROM Dosen WHERE id_user = ?"
 
-func (d *Dosen) UbahMataKuliah(matkul MataKuliah) {}
-
-func (d *Dosen) HapusMataKuliah(matkul MataKuliah) {}
-
-func (d *Dosen) LihatAbsensiMahasiswa(matkul MataKuliah) []Absensi {
-	return []Absensi{}
-}
-
-func (d *Dosen) GenerateLaporan(periode string) Laporan {
-	return Laporan{}
+	err := config.DB.QueryRow(query, id).Scan(&d.IDUser, &d.NIDN)
+	if err != nil {
+		return nil, err
+	}
+	return &d, nil
 }
