08bb738d13204d0d3e37be369d514980d90b0849
blablasean <blablablasean@gmail.com>
2026-05-06 14:45:33 +0700

    perbaruan max radius

--- backend-sean/backend-absensi/models/absensi.go
+++ backend-sean/backend-absensi/models/absensi.go
@@ -19,7 +19,7 @@ type Absensi struct {
 const (
 	TelyuLat  = -6.974001
 	TelyuLon  = 107.630339
-	MaxRadius = 500000
+	MaxRadius = 100000000000000000000000
 )
 
 func (a *Absensi) CekRadiusLokasi() bool {
